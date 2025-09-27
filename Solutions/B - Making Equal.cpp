8a52682f838501c1731b88ad788e5bdae0857c04
Apu Das Orgho <apu2723@gmail.com>
2025-09-28 00:55:01 +0600

    Update B - Making Equal.cpp

--- Solutions/B - Making Equal.cpp
+++ Solutions/B - Making Equal.cpp
@@ -1,11 +1,38 @@
 /*
-* Author    : Apu Das Orgho
+ * Author    : Apu Das Orgho
  * Problem   : Making Equal
  * Created on: 27-09-2025
  */
 #include<bits/stdc++.h>
 #define iamspeed ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
+
+
+void counFrequency(auto &a, auto &b, auto &d, auto &d2, int &id1, int &id2, int &n, bool &f) {
+    int ct = 0, ct2 = 0;
+    int x = min((id1 < n ? a[id1] : INT_MAX), (id2 < n ? b[id2] : INT_MAX));
+    for (int i = id1; i < n; i++) {
+        if (a[i] == x)ct++, id1++;
+        else break;
+    }
+    for (int i = id2; i < n; i++) {
+        if (b[i] == x)ct2++, id2++;
+        else break;
+    }
+    int tot = ct + ct2;
+    if (tot % 2) {
+        f = false;
+        return;
+    }
+    while (ct > ct2) {
+        d2.push_back(x);
+        ct -= 2;
+    }
+    while (ct2 > ct) {
+        d.push_back(x);
+        ct2 -= 2;
+    }
+}
 
 int main() {
     iamspeed
@@ -14,45 +41,24 @@ int main() {
     for (cin >> t; t--;) {
         int n;
         cin >> n;
-        vector<int> a(n);
-        map<int, int> mp;
+        vector<int> a(n), b(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
-        vector<int> b(n);
         for (int i = 0; i < n; i++) {
             cin >> b[i];
         }
         sort(a.begin(), a.end());
         sort(b.begin(), b.end());
-        for (int i = 0; i < n; i++) {
-            mp[a[i]]++;
-            mp[b[i]]--;
-        }
+        deque<int> d, d2;
+        int mn = min(a[0], b[0]) * 2, id1 = 0, id2 = 0;
         bool f = true;
-        for (auto [val,freq]: mp) {
-            if (freq % 2 != 0) {
-                f = false;
-                break;
-            }
+        while (id1 < n || id2 < n) {
+            counFrequency(a, b, d, d2, id1, id2, n, f);
         }
         if (!f) {
             cout << -1 << endl;
             continue;
-        }
-        deque<int> d, d2;
-        int mn = min(a[0], b[0]);
-        for (int i = 0; i < n; i++) {
-            auto &ache = mp[a[i]];
-            if (ache > 0) {
-                d2.push_back(a[i]);
-                ache -= 2;
-            }
-            auto &ache2 = mp[b[i]];
-            if (ache2 < 0) {
-                d.push_back(b[i]);
-                ache2 += 2;
-            }
         }
         long long int ans = 0;
         while (!d.empty()) {
@@ -61,15 +67,14 @@ int main() {
             if (df < d2f) {
                 d2.pop_back();
                 d.pop_front();
-                ans += min(2 * mn, df);
+                ans += min(mn, df);
             } else {
                 d.pop_back();
                 d2.pop_front();
-                ans += min(2 * mn, d2f);
+                ans += min(mn, d2f);
             }
         }
         cout << ans << '\n';
     }
-
     return 0;
 }
